aa783278d32917f21cd90e6c63a67b6e9cad895f
M4dCh34t3r <LegendPotty@gmail.com>
2022-06-27 14:00:02 -0300

    chore: add a 'henlo wrodl' home page

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -1,10 +1,15 @@
+import { Text, Subtext } from './Title.style'
+
 interface TitleProps {
+  text: string;
+  subtext: string;
 }
 
 export default function Title(props: TitleProps) {
   return(
     <>
+      <Text> {props.text} </Text>
+      <Subtext> {props.subtext} </Subtext>
     </>
   );
 }
-
