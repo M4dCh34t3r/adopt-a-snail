33a229087a387ac3e2268497a959579ee3ae0940
M4dCh34t3r <LegendPotty@gmail.com>
2022-06-28 11:53:51 -0300

    feat: add a 'list' to the page 'home'

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import List from "../../components/list/List";
 import Title from "../../components/title/Title";
 import Header from "../../components/header/Header";
 
@@ -5,7 +6,34 @@ export function Home() {
   return(
     <>
       <Header/>
-     <Title text="Henlo" subtext="wrodl"/>
+      <Title text="Pick a shell buddy..." subtext="And have a lot o fun together!"/>
+      <List snails={[
+            {
+              id: 1,
+              name: 'Ana',
+              description: 'Ana description',
+              image: '/ana.png'
+            },
+            {
+              id: 2,
+              name: 'Bob',
+              description: 'Bob description',
+              image: '/bob.png'
+            },
+            {
+              id: 3,
+              name: 'John',
+              description: 'John description',
+              image: '/John.png'
+            },
+            {
+              id: 4,
+              name: 'Maria',
+              description: 'Maria description',
+              image: '/maria.png'
+            }
+          ]}
+        />
     </>
   );
 }
